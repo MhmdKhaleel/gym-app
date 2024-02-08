a03a41ecd03f9c7544b76a8b631e62829aa01056
MhmdKhaleel <khaleel3936@gmail.com>
2024-02-09 00:59:33 +0530

    viewuser

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AddUser from './components/AddUser';
+import ViewUser from './components/ViewUser';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
 
           <Route path='/' element={<AddUser/>}/>
+          <Route path='/view' element={<ViewUser/>}/>
 
         </Routes>
       
